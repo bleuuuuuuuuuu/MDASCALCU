7a2afaedd3260f00c76e0b78b1a63abb383ccf3a
jhade821 <aljhadelaurentedorias@gmail.com>
2026-02-01 21:56:51 +0800

    Commit
    Implemented Division Logic

---
+++ division.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter Second Number: ");
+        double num2 = sc.nextDouble();
+
+        if (num2 == 0) {
+            System.out.println("Cannot divide by zero.");
+        } else {
+            double ans = num1 / num2;
+            System.out.println("COMPUTATION: " + num1 + " / " + num2);
+            System.out.println("Result: " + ans);
+        }
+    }
+}
